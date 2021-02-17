fb39d9080f6364da458bcdc19798d87e5e0b94bc
seymacelebi <seymaesmaa198@gmail.com>
2021-02-17 20:16:58 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kodlama.io_HW_Day4
+{
+    class MyList<T>
+    {
+        T[] items;
+        T[] tempArray;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                //items'a geri alıyoruz. 
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+            Console.WriteLine(item);
+        }
+        public int Count
+        {
+            get { return items.Length; }
+         
+        }
+        public T[] itemkey
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kodlama.io_HW_Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Şeyma");
+            isimler.Add("Şeyma");
+            isimler.Add("Şeyma");
+            isimler.Add("Şeyma");
+           Console.WriteLine("isimlerin sayısı"+isimler.Count);
+        }
+    }
+}
